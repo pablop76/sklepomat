a3ab4ab4ccb015e77e8070631dde7a665f3385d1
pablop76 <pawelpoltoraczyk@gmail.com>
2021-05-12 09:00:14 +0200

    dodanie docs dla github pages

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ const htmlmin = require("gulp-htmlmin");
 const uglify = require("gulp-uglify");
 const sass = require("gulp-sass");
 const concat = require("gulp-concat");
-const browserSync = require("browser-sync").create(); //https://browsersync.io/docs/gulp#page-top
+const browserSync = require("browser-sync").create(); //https://browsersync.io/dist/gulp#page-top
 const nunjucksRender = require("gulp-nunjucks-render");
 const autoprefixer = require('gulp-autoprefixer');
 const babel = require('gulp-babel');
@@ -23,26 +23,26 @@ const babel = require('gulp-babel');
 function imageMin(cb) {
     gulp.src("src/assets/images/*")
         .pipe(imagemin())
-        .pipe(gulp.dest("docs/images"));
+        .pipe(gulp.dest("dist/images"));
     cb();
 }
 
-// Copy all HTML files to docs
+// Copy all HTML files to dist
 function copyHTML(cb) {
-    gulp.src("src/*.html").pipe(gulp.dest("docs"));
+    gulp.src("src/*.html").pipe(gulp.dest("dist"));
     cb();
 }
 
 // Minify HTML
 function minifyHTML(cb) {
     gulp.src("src/*.html")
-        .pipe(gulp.dest("docs"))
+        .pipe(gulp.dest("dist"))
         .pipe(
             htmlmin({
                 collapseWhitespace: true
             })
         )
-        .pipe(gulp.dest("docs"));
+        .pipe(gulp.dest("dist"));
     cb();
 }
 
@@ -54,7 +54,7 @@ function js(cb) {
         }))
         .pipe(concat("main.js"))
         .pipe(uglify())
-        .pipe(gulp.dest("docs/js"));
+        .pipe(gulp.dest("dist/js"));
     cb();
 }
 
@@ -66,7 +66,7 @@ function css(cb) {
             browserlist: ['last 2 versions'],
             cascade: false
         }))
-        .pipe(gulp.dest("docs/css"))
+        .pipe(gulp.dest("dist/css"))
         // Stream changes to all browsers
         .pipe(browserSync.stream());
     cb();
@@ -80,7 +80,7 @@ function nunjucks(cb) {
                 path: ["src/templates/"] // String or Array
             })
         )
-        .pipe(gulp.dest("docs"));
+        .pipe(gulp.dest("dist"));
     cb();
 }
 
@@ -96,7 +96,7 @@ function nunjucksMinify(cb) {
                 collapseWhitespace: true
             })
         )
-        .pipe(gulp.dest("docs"));
+        .pipe(gulp.dest("dist"));
     cb();
 }
 
@@ -104,7 +104,7 @@ function nunjucksMinify(cb) {
 function watch_files() {
     browserSync.init({
         server: {
-            baseDir: "docs/"
+            baseDir: "dist/"
         }
     });
     gulp.watch("src/assets/sass/**/*.scss", css);
